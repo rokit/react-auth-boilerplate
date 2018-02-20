782d035e6e5664030192fb987bbe2eddca6fcd2f
Luke <goodvibrato@gmail.com>
2018-02-20 09:26:51 -0600

    cores

--- server/index.js
+++ server/index.js
@@ -1,26 +1,38 @@
 const express = require('express');
-// const http = require('http');
 const bodyparser = require('body-parser');
 const path = require('path');
-// const morgan = require('morgan');
-const app = express();
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const router = require('./router');
 const mongoose = require('mongoose');
-// const cors = require('cors')
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:auth/auth');
 mongoose.connection.openUri('mongodb://site:smugpaperbuttonangle@ds123658.mlab.com:23658/saga')
 
-// app.use(morgan('combined'));
-// app.use(cors());
-app.use(bodyparser.json({type: '*/*'}));
+// Multi-process to utilize all CPU cores.
+if (cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-router(app);
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
-const port = process.env.PORT || 5000;
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+} else {
+	const app = express();
+
+	app.use(bodyparser.json({type: '*/*'}));
+
+	// Priority serve any static files.
+	app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+	router(app);
+
+	const port = process.env.PORT || 5000;
+	app.listen(port, function () {
+		console.log(`Listening on port ${port}`);
+	});
+}
