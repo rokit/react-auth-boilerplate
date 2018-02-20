8acd7b13dad4cb7a742de4074068a80df33485a4
Luke <goodvibrato@gmail.com>
2018-02-20 09:00:10 -0600

    st

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const http = require('http');
 const bodyparser = require('body-parser');
+const path = require('path');
 // const morgan = require('morgan');
 const app = express();
 const router = require('./router');
@@ -13,6 +14,9 @@ mongoose.connection.openUri('mongodb://site:smugpaperbuttonangle@ds123658.mlab.c
 // app.use(morgan('combined'));
 // app.use(cors());
 app.use(bodyparser.json({type: '*/*'}));
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 router(app);
 
